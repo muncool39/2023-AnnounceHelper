2c8d38948918da8c0f65444f0fd8040fa431c989
siwon <muncool39@gmail.com>
2023-11-11 02:24:35 +0900

    first commit

---
+++ src/test/java/com/example/helper/AnnounceHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnounceHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
